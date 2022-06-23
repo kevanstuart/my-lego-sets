1cccbb92fd24b610d812a0ba77d7dd3dde6cb1ac
Kevan Stuart <kevan.jedi@gmail.com>
2022-06-23 13:56:51 +0200

    add(rebrickable) added code to fetch user token and set list from Rebrickable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import type { NextPage } from 'next'
 // import Image from 'next/image'
 
 const Home: NextPage = () => {
-  const { data, refetch, isLoading } = trpc.useQuery(['get-my-collection'], {
+  const { data, refetch, isLoading } = trpc.useQuery(['get-my-lists'], {
     refetchInterval: false,
     refetchOnReconnect: false,
     refetchOnWindowFocus: false,
@@ -15,7 +15,15 @@ const Home: NextPage = () => {
   return (
     <div className="h-screen w-screen relative">
       <main>
-        <h1>Welcome to Legoist - powered by the Rebrickable API</h1>
+        <h1 className="text-2xl text-center pt-8">Welcome to Legoist - powered by the Rebrickable API</h1>
+        <div className="text-xl text-center pt-8">
+          <h3>Please select your collection:</h3>
+          <ul>
+            { data && data.map((item: any) => (
+              <li>{item.name}</li>
+            ))}
+          </ul>
+        </div>
       </main>
     </div>
   )
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,12 +1,17 @@
+import { Rebrickable } from '@/pages/api/utils/rebrickable';
 import * as trpc from '@trpc/server';
 import { z } from 'zod';
 
+const api = new Rebrickable();
+
 export const appRouter = trpc
   .router()
-  .query('get-my-collection', {
+  .query('get-my-lists', {
     input: z.object({ text: z.string().nullish() }).nullish(),
-    async resolve({ input }) {
-      return { id: input, name: 'Bilbo' };
+    async resolve({ input }) {      
+      const setLists = await api.getSetLists();
+
+      return setLists.results;
     },
   });
 
